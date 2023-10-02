ce8f3692e668dfcf02e709380066ff2c2568f758
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-02 16:49:52 +0500

    Update 2-Text to ASCII converter.c

--- 2-Text to ASCII converter.c
+++ 2-Text to ASCII converter.c
@@ -3,4 +3,11 @@
 and also to write the program in C language that finds/prints the ASCII of every alphabet
 written in the sentence */
 
+int main(){
+  char ch;
+  printf("Enter any Character:");
+  scanf("%c",&ch);
+  printf("%d",ch);
+  return 0;
+}
 
