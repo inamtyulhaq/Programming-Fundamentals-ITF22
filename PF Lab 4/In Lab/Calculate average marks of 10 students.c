a2d28fb72484241dca0c278a4723a7ab404618ac
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-13 12:01:39 +0500

    Update Calculate average score of 10 students.c

--- PF Lab 4/In Lab/Calculate average marks of 10 students.c
+++ PF Lab 4/In Lab/Calculate average marks of 10 students.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float marks=0, count=1, average, sum;
-    printf("Enter marks of all students one by one\n");
-    while(count<=10){
-    printf("Enter mark of student no. %.0f: ", count);
+    float marks=0, count=0, average, sum;
+    printf("Enter marks of all students one by one.\n");
+    while(count<10){
+    printf("Enter mark of student no. %.0f: ", count+1);
     scanf("%f", &marks);
     sum+=marks;
     count++;
