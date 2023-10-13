669127f4eec7417345c5efae6cc13e9280402d7e
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-13 12:09:22 +0500

    Update and rename Average score of students with sentinel while loop.c to Average marks of students with sentinel while loop.c

--- PF Lab 4/In Lab/Average marks of students with sentinel while loop.c
+++ PF Lab 4/In Lab/Average marks of students with sentinel while loop.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    float marks, count=1, average, sum;
-    printf("Enter marks of all students one by one\n");
+    float marks, count=0, average, sum;
+    printf("Enter marks of all students one by one.\n");
     while(marks != -1){
-    printf("Enter mark of student no. %.0f: ", count);
+    printf("Enter mark of student no. %.f: ", count+1);
     scanf("%f", &marks);
     sum+=marks;
     count++;
     }
-    average = (sum)/10;
+    average = (sum+1)/10;
     printf("The average of all scores is %.2f\n", average);
     printf("Thanks for pressing -1. The program is closing\n");
     return 0;
