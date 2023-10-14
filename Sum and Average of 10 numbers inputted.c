553e85528274a214724d4ec60bf9bb7725453d5a
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-14 17:01:16 +0500

    Update Sum and Average of 10 numbers inputted.c

--- Sum and Average of 10 numbers inputted.c
+++ Sum and Average of 10 numbers inputted.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, count=0;
+    float average, sum;
+    while(count<10){
+        printf("Enter number %d: ", count+1);
+        scanf("%d", &num);
+        sum+=num;
+        count++;
+    }
+    printf("The sum of the 10 numbers entered is %.1f\n", sum);
+    average= (sum)/10;
+    printf("The average of the 10 numbers entered is %.2f\n", average);
+    return 0;
+}
 
