cf8ef0d900bff7fec673f15716acd37d4ba42d75
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-15 09:48:09 +0500

    Update Console based Calculator.c

--- PF Lab 4/Post Lab/Console based Calculator.c
+++ PF Lab 4/Post Lab/Console based Calculator.c
@@ -8,4 +8,34 @@ int main()
     
     printf("Please, make a choice: ");
     scanf("%d", &operation);
-    
+        
+    switch (operation)
+    {
+        case 1:
+            float num1, num2,add;
+            printf("Enter First Number: ");
+            scanf("%f", &num1);
+            getchar();
+            printf("Enter Second Number: ");
+            scanf("%f", &num2);
+            add=num1+num2;
+            printf("Addition of two numbers is: %.2f\n", add);
+            break;
+        case 2:
+            float num3, num4, sub;
+            printf("Enter First Number: ");
+            scanf("%f", &num3);
+            printf("Enter Second Number: ");
+            scanf("%f", &num4);
+            sub=num3-num4;
+            printf("Subtraction of two numbers is: %.2f\n", sub);
+            break;
+        case 3:
+            float num5, num6, multi;
+            printf("Enter First Number: ");
+            scanf("%f", &num5);
+            printf("Enter Second Number: ");
+            scanf("%f", &num6);
+            multi=num3*num4;
+            printf("Multiplication of two numbers is: %.2f\n", multi);
+            break;
