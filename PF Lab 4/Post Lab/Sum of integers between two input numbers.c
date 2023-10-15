6ce26de60518c0da0cba37002fbc481a8decec19
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-15 18:38:30 +0500

    Update Sum of integers between two input numbers.c

--- PF Lab 4/Post Lab/Sum of integers between two input numbers.c
+++ PF Lab 4/Post Lab/Sum of integers between two input numbers.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  int num1, num2, sum = 0;
+  int num1, num2,num3, sum = 0;
     printf("Enter first integer: ");
     scanf("%d", &num1);
     printf("Enter second integer: ");
     scanf("%d", &num2);
-  // Find the sum of all numbers which are dividends of 4 and 16, between these two integers
-  for (num1; num1<= num2; num1++) {
-    if (num1 % 4 == 0 && num1 % 16 == 0) {
-      sum += num1;
+  for (num3=num1; num3<num2; num3++) {
+    if (num3 % 4 == 0 && num3 % 16 == 0) {
+      sum += num3;
     }
   }
-    printf("The sum of numbers between the given two numbers divisble by 4 and 16 is %d.",sum);
+  if(sum==0){
+      printf("No such number exists between the two");
+  }
+  else {
+      printf("%d is %d",num3,sum);
+  }
+  /*else {
+      printf("The sum of numbers between the given two numbers divisble by 4 and 16 is %d",sum);
+  }*/
   return 0;
 }
