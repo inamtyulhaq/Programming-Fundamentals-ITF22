658083d89e1b2e8113bac8a7ec137770dc2e6848
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-15 09:38:23 +0500

    Update Console based Calculator.c

--- PF Lab 4/Post Lab/Console based Calculator.c
+++ PF Lab 4/Post Lab/Console based Calculator.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    int operation;
+    printf("Select an operation to perform the calculation in C Calculator:\n1. Addition               2. Subtraction\n3. Multiplication         4. Division\n5. Square                 6. Square Root\n7. Exit\n");
+    
+    printf("Please, make a choice: ");
+    scanf("%d", &operation);
+    
