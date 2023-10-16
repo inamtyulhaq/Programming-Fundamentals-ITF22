ac58f42e79465cd1b173809366c4fe890ce9bd84
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-16 09:44:15 +0500

    Update Sum of integers between two input numbers.c

--- PF Lab 4/Post Lab/Sum of integers between two input numbers.c
+++ PF Lab 4/Post Lab/Sum of integers between two input numbers.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  int num1, num2,sum = 0,i,j;
+  int num1, num2,sum = 0,i,j=1;
     printf("Enter first integer: ");
     scanf("%d", &num1);
     printf("Enter second integer: ");
     scanf("%d", &num2);
   for (i = num1; i <= num2; i++) {
     if (i % 4 == 0 && i % 16 == 0) {
+        if(j){
+            printf("%d", i);
+            j=0;/*so that the else condition then takes place and + is printed*/
+        }
+        else{
+            printf(" + %d",  i);
+        }
       sum += i;
-      printf("%d+",i);//has a problem
     }
   }
   if(sum==0){
