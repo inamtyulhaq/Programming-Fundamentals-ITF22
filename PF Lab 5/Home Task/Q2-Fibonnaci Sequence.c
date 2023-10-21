b2bfe12e19c33ef0337a490d9cb67e5206cebae9
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-21 14:22:02 +0500

    Update Q2-Fibonnaci Sequence.c

--- PF Lab 5/Home Task/Q2-Fibonnaci Sequence.c
+++ PF Lab 5/Home Task/Q2-Fibonnaci Sequence.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(){
+    
+    int n,num0=0,num1=1,num2;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    if(n>=0){ //input validation added
+    for(int i=0;i<n;i++){
+        printf("%d ",num0);
+        num2=num0+num1;
+        num0=num1;
+        num1=num2;
+    }
+    }
+    else{
+        printf("This number isn't positive.");
+    }
+    return 0;
+}
