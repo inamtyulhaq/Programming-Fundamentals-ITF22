f24b674557e61351b88f12251c3a36a50f0fe8e8
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-11-13 22:01:31 +0500

    Update newfile.c

--- newfile.c
+++ newfile.c
@@ -1,1 +1,18 @@
 //pf lab 5 code
+#include <stdio.h>
+int main(){
+    int multi(int a, int b){
+        int c;
+        c = a * b;
+        return c;
+    }
+int x,y;
+printf("Enter x: ");
+scanf("%d", &x);
+printf("Enter y: ");
+scanf("%d", &y);
+int z =  multi(x,y);
+printf("The product of x and y is %d", z);
+    return 0;
+}
+
